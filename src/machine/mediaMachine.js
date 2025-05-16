9ef35bb0be4bf2c5c13d3aa97f9938f703d2ada3
redVi <liberty195@gmail.com>
2025-05-16 03:13:44 +0300

    eslint

--- src/machine/mediaMachine.js
+++ src/machine/mediaMachine.js
@@ -1,92 +1,92 @@
 import { createMachine, assign } from 'xstate';
 
 export const mediaMachine = createMachine({
-  id: 'media',
-  initial: 'closed',
-  context: {
-    videoRef: null,
-  },
-  states: {
-    closed: {
-      entry: ['pauseVideo'],
-      on: {
-        OPEN: {
-          target: 'opened.full.playing',
-          actions: assign({ videoRef: ({ event }) => event.videoRef })
-        }
-      }
+    id: 'media',
+    initial: 'closed',
+    context: {
+        videoRef: null,
     },
-    opened: {
-      initial: 'full',
-      states: {
-        full: {
-          initial: 'playing',
-          states: {
-            playing: {
-              entry: ['playVideo'],
-              on: {
-                TOGGLE_PLAY: {
-                  target: 'paused',
-                  actions: ['pauseVideo']
+    states: {
+        closed: {
+            entry: ['pauseVideo'],
+            on: {
+                OPEN: {
+                    target: 'opened.full.playing',
+                    actions: assign({ videoRef: ({ event }) => event.videoRef })
                 }
-              }
-            },
-            paused: {
-              on: {
-                TOGGLE_PLAY: {
-                  target: 'playing',
-                  actions: ['playVideo']
-                }
-              }
             }
-          },
-          on: {
-            TOGGLE_SIZE: 'mini',
-            CLOSE: {
-              target: '#media.closed',
-              actions: ['pauseVideo']
-            }
-          }
         },
-        mini: {
-          initial: 'playing',
-          states: {
-            playing: {
-              entry: ['playVideo'],
-              on: {
-                TOGGLE_PLAY: {
-                  target: 'paused',
-                  actions: ['pauseVideo']
+        opened: {
+            initial: 'full',
+            states: {
+                full: {
+                    initial: 'playing',
+                    states: {
+                        playing: {
+                            entry: ['playVideo'],
+                            on: {
+                                TOGGLE_PLAY: {
+                                    target: 'paused',
+                                    actions: ['pauseVideo']
+                                }
+                            }
+                        },
+                        paused: {
+                            on: {
+                                TOGGLE_PLAY: {
+                                    target: 'playing',
+                                    actions: ['playVideo']
+                                }
+                            }
+                        }
+                    },
+                    on: {
+                        TOGGLE_SIZE: 'mini',
+                        CLOSE: {
+                            target: '#media.closed',
+                            actions: ['pauseVideo']
+                        }
+                    }
+                },
+                mini: {
+                    initial: 'playing',
+                    states: {
+                        playing: {
+                            entry: ['playVideo'],
+                            on: {
+                                TOGGLE_PLAY: {
+                                    target: 'paused',
+                                    actions: ['pauseVideo']
+                                }
+                            }
+                        },
+                        paused: {
+                            on: {
+                                TOGGLE_PLAY: {
+                                    target: 'playing',
+                                    actions: ['playVideo']
+                                }
+                            }
+                        }
+                    },
+                    on: {
+                        TOGGLE_SIZE: 'full',
+                        CLOSE: {
+                            target: '#media.closed',
+                            actions: ['pauseVideo']
+                        }
+                    }
                 }
-              }
-            },
-            paused: {
-              on: {
-                TOGGLE_PLAY: {
-                  target: 'playing',
-                  actions: ['playVideo']
-                }
-              }
             }
-          },
-          on: {
-            TOGGLE_SIZE: 'full',
-            CLOSE: {
-              target: '#media.closed',
-              actions: ['pauseVideo']
-            }
-          }
         }
-      }
     }
-  }
 }, {
-  actions: {
-    playVideo: ({ context }) => {
-      context.videoRef?.current?.play();
-    },
-    pauseVideo: ({ context }) => {
-      context.videoRef?.current?.pause();
+    actions: {
+        playVideo: ({ context }) => {
+            context.videoRef?.current?.play();
+        },
+        pauseVideo: ({ context }) => {
+            context.videoRef?.current?.pause();
+        }
     }
-  }
 });
